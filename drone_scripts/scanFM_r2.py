7e3c3cda1c39138249538e012788330c5ae8d290
Stephen Wayne <swayne275@gmail.com>
2017-02-07 11:13:26 -0800

    Added DTV-B Tuner SDR scan
    sdrClass is a class for a DTV-B tuner using the RTL2832U chipset and
    R820T tuner. scanFM is just a driver script to do some basic RF scanning

---
+++ drone_scripts/scanFM_r2.py
@@ -0,0 +1,68 @@
+'''
+Author: Stephen Wayne
+This file exists mostly to test features that I add to the rxSDR class
+
+Can now smell radio waves. Plots speed improved by 2-3x.
+1/24:    changed freq range to BW around center freq so useful to scan
+1/26:    adding ability to print to a file
+'''
+
+
+import sdrClass_r1
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+
+plt.close('all')
+
+np.set_printoptions(precision=4)
+
+fcLow = 30
+fcHigh = 1750
+fs = 2.5
+bw = 300
+gain = 'auto'
+NFFT = 1024
+PLOT = False
+SAVE = True
+FILENAME = "fullRange_2_5mhz.txt"
+NUM_DECIMAL = 3
+SCAN_RES = 1
+
+radio = sdrClass_r1.rxSDR(30, fs, bw, gain)  # radio on RPi
+
+fc_list = np.linspace(fcLow, fcHigh, ((fcHigh - fcLow)/(SCAN_RES*fs) + 1))
+
+# this plots the data 2-3x faster than usual. useful for obs. single freq
+if PLOT:
+    plt.clf();
+    plt.ion()                             # turn on interactive plotting
+    fLow = -0.5*fs
+    fHigh = 0.5*fs                        # plot BW to scan many freqs
+    f = np.linspace(fLow, fHigh, NFFT)
+    fig, ax= plt.subplots()
+    line, = ax.plot(np.random.randn(100))
+    plt.title("Spectrum at fc=" + str(fcLow) + ", fs=" + str(fs*1000) + " khz")
+    plt.ylabel("magnitude (dB)")
+    plt.xlabel("frequency bin")
+    plt.xlim(fLow, fHigh)                     # observe entire bandwidth
+    plt.ylim(-30, 30)                         # sets plot dynamic range, in dB
+    plt.show(block=False)
+    line.set_xdata(f)
+if SAVE:
+    with open(FILENAME, 'a') as file:
+        file.write('fl,%f,fh,%f,fs,%f,mhz,gain,%s,nfft,%d,scan,%f'%(fcLow,fcHigh,fs,gain,NFFT,SCAN_RES)+'\n')
+now = time.time()
+for x in fc_list:
+    radio.setFc(x, "mhz")
+    if ((time.time() - now) < 0.025): time.sleep(0.025 - (time.time()-now)) # allow PLL to settle
+    freqs = radio.getFrequencies(NFFT)
+    if PLOT:
+        line.set_ydata(freqs)
+        plt.title("Spectrum at f=" + str(x) + ", fs=" + str(fs*1000) + " khz")
+        fig.canvas.draw()
+        fig.canvas.flush_events()
+    if SAVE:
+        with open(FILENAME, 'a') as f:
+            f.write(','.join(map(str, np.round(freqs, NUM_DECIMAL))) + '\n')
+print(time.time() - now)
